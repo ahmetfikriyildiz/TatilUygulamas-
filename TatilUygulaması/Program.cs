3c954496819963a42c5d70cfe77c1ddd1aa808be
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-10-06 13:55:04 +0300

    .

--- TatilUygulaması/Program.cs
+++ TatilUygulaması/Program.cs
@@ -10,6 +10,10 @@ namespace TatilUygulaması
     {
         static void Main(string[] args)
         {
+
+            // Bu pratikte bir yardımcı seyehat uygulaması ile kullanıcılarımızın planlayacakları 3 günlük bir tatilde harcayacakları yaklaşık tutarı hesaplamalarına yardımcı oluyoruz.
+
+
             bool anotherVacation = true;
             while (anotherVacation)
             {
